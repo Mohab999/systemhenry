a057db76e5f9e4459ecb05bcfdb456b8049f14e1
Mohab999 <mohab@gmail.com>
2018-11-29 20:55:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -715,7 +715,7 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
-const adminprefix = "-";
+const adminprefix = "++";
 const devs = ['516226504066072588'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
